f2206e605477e0e905c19044f4a269b597074ffa
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-02-06 15:07:53 -0300

    aula 16 projeto final

--- clone-twitter/assets/js/script.js
+++ clone-twitter/assets/js/script.js
@@ -1,11 +1,31 @@
-function imprimirNoConsole(event){
+function pegarTweet(event){
     event.preventDefault();
     const tweetTextArea = textarea.value;
-    console.log(tweetTextArea);
+    criarTweet(tweetTextArea);
+}
+
+function criarTweet(tweetTexto){
+    let data = new Date();
+    let hora = data.getHours();
+    let minutos = data.getMinutes();
+
+    const tweet = {
+        nome: "Daniel",
+        foto: "./src/ProfilePic.png",
+        usuario: "@danielkenzie",
+        texto: tweetTexto,
+        tempo: `${hora}:${minutos}`
+    }
+
+    listarTweet(tweet);
+}
+
+function listarTweet(tweet){
+
 }
 
 const tweetar = document.querySelector("button");
 const textarea = document.querySelector("textarea");
 
-tweetar.addEventListener("click", imprimirNoConsole);
+tweetar.addEventListener("click", pegarTweet);
 
